ec329d6c09670bf9ca0b50b4ab4050bca74fd9d9
LaraJaques <larastefanyjaques@gmail.com>
2025-10-24 21:06:54 -0300

    Create atividade12.js

---
+++ atividade12.js
@@ -0,0 +1,15 @@
+//variavel nome usando const
+const nome="Lara";
+//variavel idade usando let
+let idade=18;
+idade=19;
+//crie um objeto produto usando const,com propriedade nome e valor
+const produto= {
+    nome: "camiseta",
+    valor: 50
+};
+//modificar o valor
+produto.valor=80;
+console.log(nome);
+console.log(idade);
+console.log(produto);
