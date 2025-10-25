e44c710d0ddc9ce731a18c84131d7043a4a28d01
LaraJaques <larastefanyjaques@gmail.com>
2025-10-24 21:08:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Esse é meu primeiro script js");
+document.getElementByld("titulo").innerText="Texto alterado pelo JavaScript";
+document.getElementByld("meuBotão").addEventListenr("clique",function(){
+    alert("Você clicpu no botão!");
+});
